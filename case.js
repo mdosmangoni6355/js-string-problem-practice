24e816fa8ded2e3431acdb7bba2c51fd974be41c
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-08-07 02:07:22 +0600

    case.js file added

---
+++ case.js
@@ -0,0 +1,18 @@
+let firstName = "OsmanGonI";
+let lastName = "OsmanGoni";
+
+// firstName = firstName.toLowerCase();
+// lastName = lastName.toLowerCase();
+
+firstName = firstName.toUpperCase();
+lastName = lastName.toUpperCase();
+
+console.log(firstName);
+console.log(lastName);
+
+if (firstName === lastName) {
+  console.log("Valid Name");
+}
+else {
+  console.log("Invalid Name");
+}
